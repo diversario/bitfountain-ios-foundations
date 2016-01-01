154e77158af745a5c4706d60c56c9a4bf5177e1d
Ilya Shaisultanov <ishaisultanov@shutterstock.com>
2016-01-01 10:35:40 -0800

    Put everything in one repo

---
+++ iOS/AnimalTrivia/AnimalTrivia/ViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  AnimalTrivia
+//
+//  Created by Ilya Shaisultanov on 12/30/15.
+//  Copyright © 2015 Ilya Shaisultanov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var aLabel: UILabel!
+    @IBOutlet weak var bLabel: UILabel!
+    @IBOutlet weak var cLabel: UILabel!
+    
+    @IBOutlet weak var aButton: UIButton!
+    @IBOutlet weak var bButton: UIButton!
+    @IBOutlet weak var cButton: UIButton!
+    
+    @IBOutlet weak var incorrectAImageView: UIImageView!
+    @IBOutlet weak var incorrectBImageView: UIImageView!
+    @IBOutlet weak var correctCImageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func aButtonPressed(sender: UIButton) {
+    }
+
+    @IBAction func bButtonPressed(sender: UIButton) {
+    }
+    
+    @IBAction func cButtonPressed(sender: UIButton) {
+    }
+    
+}
+
