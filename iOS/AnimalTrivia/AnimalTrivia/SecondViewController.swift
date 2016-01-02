f830c3241776c7f3aadea7eb3211f5c9225af501
Ilya Shaisultanov <ishaisultanov@shutterstock.com>
2016-01-01 22:16:06 -0800

    Animal Trivia works! 🤗

--- iOS/AnimalTrivia/AnimalTrivia/SecondViewController.swift
+++ iOS/AnimalTrivia/AnimalTrivia/SecondViewController.swift
@@ -1,14 +1,14 @@
 //
-//  ViewController.swift
+//  SecondViewController.swift
 //  AnimalTrivia
 //
-//  Created by Ilya Shaisultanov on 12/30/15.
-//  Copyright © 2015 Ilya Shaisultanov. All rights reserved.
+//  Created by Ilya Shaisultanov on 1/1/16.
+//  Copyright © 2016 Ilya Shaisultanov. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+class SecondViewController: UIViewController {
   
   @IBOutlet weak var aLabel: UILabel!
   @IBOutlet weak var bLabel: UILabel!
@@ -19,12 +19,13 @@ class ViewController: UIViewController {
   @IBOutlet weak var cButton: UIButton!
   
   @IBOutlet weak var incorrectAImageView: UIImageView!
-  @IBOutlet weak var incorrectBImageView: UIImageView!
-  @IBOutlet weak var correctCImageView: UIImageView!
+  @IBOutlet weak var correctBImageView: UIImageView!
+  @IBOutlet weak var incorrectCImageView: UIImageView!
   
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    // Do any additional setup after loading the view.
   }
   
   override func didReceiveMemoryWarning() {
@@ -50,18 +51,17 @@ class ViewController: UIViewController {
   }
   
   @IBAction func bButtonPressed(sender: UIButton) {
-    incorrectBImageView.hidden = false
+    correctBImageView.hidden = false
     bButton.hidden = true
-    bLabel.textColor = UIColor.redColor()
+    bLabel.textColor = UIColor.greenColor()
     disableButtons()
   }
   
   @IBAction func cButtonPressed(sender: UIButton) {
-    correctCImageView.hidden = false
+    incorrectCImageView.hidden = false
     cButton.hidden = true
-    cLabel.textColor = UIColor.greenColor()
+    cLabel.textColor = UIColor.redColor()
     disableButtons()
   }
   
 }
-
