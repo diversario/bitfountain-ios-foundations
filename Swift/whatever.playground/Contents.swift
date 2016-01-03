fc00760f44b3f3c02dd9e688b89e790ea55ae2fa
Ilya Shaisultanov <ishaisultanov@shutterstock.com>
2016-01-02 16:39:55 -0800

    Tip calculator!

---
+++ Swift/whatever.playground/Contents.swift
@@ -0,0 +1,17 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+UITextField()
+
+let a: Int? = 3
+let b = 3
+
+a! == b
+
+/*: whatever
+### asd
+* xzxcv
+*/
